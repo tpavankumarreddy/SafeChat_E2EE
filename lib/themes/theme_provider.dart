0c89a518257d51e5c9719fcc216d0b2334b4c27c
Pavan Kumar Reddy T <tpavankumarreddy2004@gmail.com>
2024-08-08 22:45:09 +0530

    Implemented dynamic colours that sync with wallpaper.

--- lib/themes/theme_provider.dart
+++ lib/themes/theme_provider.dart
@@ -1,25 +1,35 @@
-import 'package:emailchat/themes/light_mode.dart';
 import 'package:flutter/material.dart';
+
 import 'darkmode.dart';
+import 'light_mode.dart';
 
 class ThemeProvider extends ChangeNotifier {
   ThemeData _themeData = lightMode;
+  ColorScheme? _dynamicColorScheme;
 
-  ThemeData get themedata =>_themeData;
+  ThemeData get themedata {
+    if (_dynamicColorScheme != null) {
+      return ThemeData(
+        colorScheme: _dynamicColorScheme!,
+        useMaterial3: true,
+      );
+    }
+    return _themeData;
+  }
 
   bool get isDarkmode => _themeData == darkMode;
 
-  set themeData(ThemeData themeData) {
-    _themeData = themeData;
+  void setDynamicColorScheme(ColorScheme? colorScheme) {
+    _dynamicColorScheme = colorScheme;
     notifyListeners();
-
   }
+
   void toggleTheme() {
     if (_themeData == lightMode) {
-      themeData = darkMode;
-    }else{
-      themeData = lightMode;
-
+      _themeData = darkMode;
+    } else {
+      _themeData = lightMode;
     }
+    notifyListeners();
   }
 }

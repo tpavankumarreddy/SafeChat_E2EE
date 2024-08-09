dab1d1fe1f41ee57b021846e2ffc37e7b4215e9f
hemesh <hemesh66584@gmail.com>
2024-08-09 09:52:18 +0530

    Updated text field ui.

--- lib/components/my_textfield.dart
+++ lib/components/my_textfield.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 
-class MyTextField extends StatelessWidget{
+class MyTextField extends StatelessWidget {
   final String hintText;
   final bool obscuredText;
   final TextEditingController controller;
 
-  const MyTextField({super.key,
+  const MyTextField({
+    super.key,
     required this.hintText,
     required this.obscuredText,
     required this.controller,
-  }
-  );
+  });
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 25.0),
       child: TextField(
@@ -21,16 +21,17 @@ class MyTextField extends StatelessWidget{
         controller: controller,
         decoration: InputDecoration(
           enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Theme.of(context).colorScheme.tertiary),
+            borderSide: BorderSide(color: Theme.of(context).colorScheme.primary),
           ),
           focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Theme.of(context).colorScheme.primary),
+            borderSide: BorderSide(color: Theme.of(context).colorScheme.secondary),
           ),
-          fillColor: Theme.of(context).colorScheme.tertiary,
+          fillColor: Theme.of(context).colorScheme.surface, // Adjusted fill color
           filled: true,
           hintText: hintText,
-          hintStyle: TextStyle(color: Theme.of(context).colorScheme.secondary),
+          hintStyle: TextStyle(color: Theme.of(context).colorScheme.onSurface), // Adjusted hint color
         ),
+        style: TextStyle(color: Theme.of(context).colorScheme.onSurface), // Ensure text is visible
       ),
     );
   }

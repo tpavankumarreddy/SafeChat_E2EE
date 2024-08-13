4bc20ca983dfc01e27788915e33c161686fddfbd
Pavan Kumar Reddy T <tpavankumarreddy2004@gmail.com>
2024-08-13 12:28:34 +0530

    updated register_page.dart to support for new OTP Page.

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -6,6 +6,7 @@ import 'package:emailchat/components/my_textfield.dart';
 import 'package:flutter/material.dart';
 import 'package:email_otp/email_otp.dart';
 import '../services/auth/otp_service.dart';
+import 'otp_page.dart';
 
 class RegisterPage extends StatelessWidget {
   EmailOTP myAuth = EmailOTP();
@@ -14,7 +15,6 @@ class RegisterPage extends StatelessWidget {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _pwController = TextEditingController();
   final TextEditingController _confirmPwController = TextEditingController();
-  final TextEditingController _otpController = TextEditingController();
 
   final void Function()? onTap;
 
@@ -22,11 +22,11 @@ class RegisterPage extends StatelessWidget {
 
   // Timer related fields
   Timer? _timer;
-  int _remainingTime = 60; // 1 minute
+  int _remainingTime = 90;
   bool _showResendButton = false;
 
   Future<void> _startTimer(VoidCallback onResendAvailable) async {
-    _remainingTime = 60; // reset timer to 1 minute
+    _remainingTime = 90; // reset timer to 90 sec
     _showResendButton = false;
 
     _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
@@ -46,8 +46,6 @@ class RegisterPage extends StatelessWidget {
   }
 
   Future<void> register(BuildContext context) async {
-    final auth = AuthService();
-    final otpService = OTPService();
 
     if (_nameController.text.isEmpty ||
         _emailController.text.isEmpty ||
@@ -89,143 +87,89 @@ class RegisterPage extends StatelessWidget {
     }
 
     if (_pwController.text == _confirmPwController.text && _pwController.text.length >= 6) {
-      try {
-        await otpService.sendOTP(myAuth, _emailController.text, _nameController.text);
 
-        await _startTimer(() {
-          // If the timer expires, close the OTP dialog
-          Navigator.of(context).pop();
-        });
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => OTPPage(
+            email: _emailController.text,
+            username: _nameController.text,
+            onOTPVerified: () async {
+              final auth = AuthService();
+              try {
+                final returnedOTP = await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => OTPPage(
+                      email: _emailController.text,
+                      username: _nameController.text,
+                      onOTPVerified: () {},
+                    ),
+                  ),
+                );
 
-        bool otpVerified = false;
+                if (returnedOTP != null) {
+                  try {
+                  await auth.signUpWithEmailPassword(_emailController.text, _pwController.text, returnedOTP);
 
-        await showDialog(
-          context: context,
-          barrierDismissible: false,
-          builder: (context) {
-            return StatefulBuilder(
-              builder: (context, setState) {
-                return AlertDialog(
-                  title: const Text('Enter OTP'),
-                  content: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      if (!_showResendButton) // Show OTP text field only if time hasn't expired
-                        TextField(
-                          controller: _otpController,
-                          decoration: const InputDecoration(labelText: 'OTP'),
-                        ),
-                      const SizedBox(height: 20),
-                      if (_showResendButton)
-                        Column(
-                          children: [
-                            const Text('Time expired. Please click "Resend" to get a new OTP.'),
+                  Navigator.pop(context); // Navigate back to RegisterPage
+                  showDialog(
+                    context: context,
+                    builder: (context) => const SnackBar(
+                      content: Text("Registration Successful! 🥳. Your account has been created."),
+                  ),
+                );
+                } on Exception catch (e) {
+                    if (e.toString().contains('email-already-in-use')) {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: const Text("Email already in use"),
+                          content: const Text("This email is already registered. Please login instead."),
+                          actions: [
                             TextButton(
-                              onPressed: () async {
-                                await _resendOTP();
-                                setState(() {
-                                  _showResendButton = false;
-                                  _remainingTime = 60; // Reset the timer
-                                });
+                              onPressed: () {
+                                Navigator.pop(context); // Dismiss the dialog
+                                onTap?.call(); // Navigate to login page
                               },
-                              child: const Text('Resend OTP'),
+                              child: const Text('Login'),
                             ),
                           ],
-                        )
-                      else
-                        Text('OTP expires in $_remainingTime seconds'),
-                    ],
+                        ),
+                      );
+                    }
+                    else {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: const Text("Registration Error"),
+                          content: Text(e.toString()),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: const Text('Ok'),
+                            ),
+                          ],
+                        ),
+                      );
+                    }
+                  }
+              }
+              }catch (e) {
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: Text(e.toString()),
                   ),
-                  actions: [
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: const Text('Cancel'),
-                    ),
-                    if (!_showResendButton) // Only show Verify button if time hasn't expired
-                      TextButton(
-                        onPressed: () async {
-                          final value = _otpController.text;
-                          if (EmailOTP.verifyOTP(otp: value)) {
-                            otpVerified = true;
-                            Navigator.pop(context); // Close the OTP dialog
-                          } else {
-                            showDialog(
-                              context: context,
-                              builder: (context) => AlertDialog(
-                                title: const Text("Invalid OTP.",
-                                    style: TextStyle(fontSize: 18)),
-                                actions: [
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                    },
-                                    child: const Text('Ok'),
-                                  ),
-                                ],
-                              ),
-                            );
-                          }
-                        },
-                        child: const Text('Verify'),
-                      ),
-                  ],
                 );
-              },
-            );
-          },
-        );
-
-        if (otpVerified) {
-          try {
-            await auth.signUpWithEmailPassword(_emailController.text, _pwController.text, _otpController.text);
-          } on Exception catch (e) {
-            if (e.toString().contains('email-already-in-use')) {
-              showDialog(
-                context: context,
-                builder: (context) => AlertDialog(
-                  title: const Text("Email already in use"),
-                  content: const Text("This email is already registered. Please login instead."),
-                  actions: [
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pop(context); // Dismiss the dialog
-                        onTap?.call(); // Navigate to login page
-                      },
-                      child: const Text('Login'),
-                    ),
-                  ],
-                ),
-              );
-            } else {
-              showDialog(
-                context: context,
-                builder: (context) => AlertDialog(
-                  title: const Text("Registration Error"),
-                  content: Text(e.toString()),
-                  actions: [
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: const Text('Ok'),
-                    ),
-                  ],
-                ),
-              );
-            }
-          }
-        }
-
-      } catch (e) {
-        showDialog(
-          context: context,
-          builder: (context) => AlertDialog(
-            title: Text(e.toString()),
+              }
+            },
           ),
-        );
-      }
+        ),
+      );
+
     } else if (_pwController.text != _confirmPwController.text) {
       showDialog(
         context: context,
@@ -306,7 +250,7 @@ class RegisterPage extends StatelessWidget {
             ),
             const SizedBox(height: 25),
             MyButton(
-              text: "Register",
+              text: "R E G I S T E R",
               onTap: () => register(context),
             ),
             const SizedBox(height: 20),

34d7dd2789285222b970467fb1fab8a69411fe3a
Pavan Kumar Reddy T <tpavankumarreddy2004@gmail.com>
2024-10-15 00:50:40 +0530

    Android Gradle Plugin updated.

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -4,9 +4,11 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   cloud_firestore
+  dynamic_color
   firebase_auth
   firebase_core
   flutter_secure_storage_windows
+  local_auth_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
